7b85c2db3a1af96b28e63d77537055d0f3a4150f
Dhiren Chugh <85895240+dce23-git@users.noreply.github.com>
2021-09-04 10:35:30 +0530

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    cout << "This is a simple C++ Program!\n";
+    cout<<"Enter a number to check if it is odd or even\n";
+    int n;
+    cin >> n;
+    if(n%2)cout<<"Odd"<<endl;
+    else cout<<"Even"<<endl;
+}
